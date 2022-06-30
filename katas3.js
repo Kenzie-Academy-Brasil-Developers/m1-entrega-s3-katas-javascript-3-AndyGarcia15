01eb58c909d1c024aaee375dc118d605498dbc05
Andy Garcia| <andy_gar@yahoo.com>
2022-06-29 21:43:13 -0300

    atualização

--- katas3.js
+++ katas3.js
@@ -1,48 +1,121 @@
 const sampleArray = [469, 755, 244, 245, 758, 450, 302, 17, 712, 71, 456, 21, 398, 339, 882, 848, 179, 535, 940, 472];
 
-function kata1() {
-    // implemente o código do kata 1 aqui
-}
+function kata1(num) {
+    let resultado = [];
+    
+    for(let i = 1; i <= num; i++ ){
+        resultado  = console.log(i)
+    }
+    return resultado
+}   
+// kata1(25)
 
-function kata2() {
-    // implemente o código do kata 2 aqui
+function kata2(num) {
+    let resultado = num
+    
+    for(let i = num; 1 <= i; i-- ){
+        resultado = console.log(i)
+    }
+    return resultado
 }
+// kata2(23)
 
-function kata3() {
-    // implemente o código do kata 3 aqui
+function kata3(num) {
+    let resultado = num
+    
+    for(let i = -1; num <= i; i-- ){
+        resultado = console.log(i)
+    }
+    return resultado
 }
+// kata3(-25)
+function kata4(num) {
+    let resultado = num
+    
+    for(let i = num; i <= -1; i++ ){
+        resultado = console.log(i)
+    }
+    return resultado
+}
+ // kata4(-25)
 
-function kata4() {
-    // implemente o código do kata 4 aqui
+function kata5(num) {
+    let resultado = num
+    
+    for(let i = num; i >= -25; i-- ){
+        if(i % 2 != 0){
+            resultado = console.log(i)
+        }
+    }
+    return resultado
 }
+// kata5(25)
 
-function kata5() {
-    // implemente o código do kata 5 aqui
+function kata6(num) {
+    let resultado = num
+    
+    for(let i = 3; i <= num; i++ ){
+        if(i % 3 == 0){
+            resultado = console.log(i)
+        }
+    }
+    return resultado
 }
+// kata6(100)
 
-function kata6() {
-    // implemente o código do kata 6 aqui
+function kata7(num) {
+    let resultado = num
+    
+    for(let i = 7; i <= num; i++ ){
+        if(i % 7 == 0){
+            resultado = console.log(i)
+        }
+    }
+    return resultado
 }
+// kata7(100)
 
-function kata7() {
-    // implemente o código do kata 7 aqui
+function kata8(num) {
+    let resultado = num
+    
+    for(let i = num; i >= 3; i-- ){
+        if(i % 7 == 0){
+            resultado = console.log(i)
+        }
+        if(i % 3 == 0){
+            resultado = console.log(i)
+        }
+    }
+    return resultado
 }
+// kata8(100)
 
-function kata8() {
-    // implemente o código do kata 8 aqui
+function kata9(num) {
+    let resultado = num
+    
+    for(let i = 5; i <= num; i++ ){
+        if(i % 5 == 0 && i % 2 != 0){
+            resultado = console.log(i)
+        }
+    }
+    return resultado
 }
+// kata9(100)
 
-function kata9() {
-    // implemente o código do kata 9 aqui
+function kata10(arr) {
+    return console.log(arr)
 }
-
-function kata10() {
-    // implemente o código do kata 10 aqui
+// kata10(sampleArray)
+function kata11(arr) {
+    let impares = []
+    for(let i = 0; i < arr.length; i++){
+        if(i % 2 != 0){
+           impares = impares.push(i)
+        }
+    }
+    return console.log(impares)
 }
-
-function kata11() {
-    // implemente o código do kata 11 aqui
-}
+ kata11(sampleArray)
 
 function kata12() {
     // implemente o código do kata 12 aqui
